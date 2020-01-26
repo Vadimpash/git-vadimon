9e9b2ec661112d2a97ad85ae715de4e45a317d20
Vadimpash <pash.vadim.99@bk.ru>
2020-01-26 22:16:26 +0300

    Немного адаптировал проект

--- src/components/ticker/ticker.js
+++ src/components/ticker/ticker.js
@@ -4,9 +4,11 @@ import './ticker.css';
 const Ticker = () => {
     return (
         <div className="ticker">
+            <span>
             <marquee behavior="scroll" direction="left">Уважаемые пациенты! При обращении в медицинский центр вам
                 необходимо предоставить документ удостоверящий личность.
             </marquee>
+                </span>
         </div>
     );
 }
